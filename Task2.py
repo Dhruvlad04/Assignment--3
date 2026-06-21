475bcd45b14fe04a586d2b1660843cce2e329060
Dhruvlad04 <aapu49260@gmail.com>
2026-06-21 10:34:49 +0530

    Create Task2.py

---
+++ Task2.py
@@ -0,0 +1,10 @@
+#math module is imported to use the mathematical functions
+import math 
+num=int(input("Enter a number: "))
+# square root, natural logarithm and sine of a number is calculated using the math module
+sqrt=math.sqrt(num)
+print("The square root of",num,"is",sqrt)
+log=math.log(num)
+print("The natural logarithm of",num,"is",log)  
+sine=math.sin(num)  
+print("The sine of",num,"is",sine)  
